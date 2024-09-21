c050aa55fde42ac1c232340956a2d4221f3b1596
pontaGC <makizawawa@gmail.com>
2024-09-21 21:23:40 +0900

    Create DependencyRegistrant.

---
+++ src/XsdTool.Shared/DependencyInjection/DependencyInjectionBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace XsdTool.Shared.DependencyInjection;
+
+/// <summary>
+/// The abstract implementation of <see cref="IDependenyRegistrant"/>.
+/// </summary>
+public abstract class DependencyInjectionBase : IDependenyRegistrant
+{
+    void IDependenyRegistrant.Register(IServiceCollection container)
+    {
+        ArgumentNullException.ThrowIfNull(nameof(container));
+        this.Register(container);
+    }
+
+    /// <summary>
+    /// Registers the services to container.
+    /// </summary>
+    /// <param name="container">The DI container.</param>
+    protected abstract void Register([NotNull] IServiceCollection container);
+}
---
+++ src/XsdTool.Shared/DependencyInjection/IDependenyRegistrant.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace XsdTool.Shared.DependencyInjection;
+
+/// <summary>
+/// The registrant that registers the dependencies to a container.
+/// </summary>
+public interface IDependenyRegistrant
+{
+    /// <summary>
+    /// Registers the services to container.
+    /// </summary>
+    /// <param name="container">The target container to register.</param>
+    /// <exception cref="ArgumentNullException"><c>container</c> is <c>null</c>.</exception>
+    void Register(IServiceCollection container);
+}
