01cfc9601c25f118bba16a9dfaebae84b6f1abf6
pontaGC <makizawawa@gmail.com>
2024-09-21 20:53:51 +0900

    Create MAUI solution.

---
+++ src/XsdTool/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XsdTool
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/XsdTool/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace XsdTool
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
